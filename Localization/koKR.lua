2500efb9b7997ca600fa8c439ce59ac07d363253
Archarodim <Archarodim@teaser.fr>
2009-01-29 23:35:26 +0100

    koKR update from Chkid (Korea)

--- Localization/koKR.lua
+++ Localization/koKR.lua
@@ -1,4 +1,4 @@
-﻿--[[
+--[[
     Decursive (v 2.0) add-on for World of Warcraft UI
     Copyright (C) 2006-2007 John Wellesz (Archarodim) ( http://www.2072productions.com/?to=decursive.php )
     This is the continued work of the original Decursive (v1.9.4) by Quu
@@ -21,43 +21,30 @@
 --]]
 -------------------------------------------------------------------------------
 
+
 if not DcrLoadedFiles or not DcrLoadedFiles["enUS.lua"] then
     if not DcrCorrupted then message("Decursive installation is corrupted! (enUS.lua not loaded)"); end;
     DcrCorrupted = true;
     return;
 end
 
+-------------------------------------------------------------------------------
+-- Korean localization
+-------------------------------------------------------------------------------
 --Dcr:SetDateAndRevision("$Date: 2008-07-22 06:35:49 +0200 (mar., 22 juil. 2008) $", "$Revision: 78889 $");
-
 
 -- Acelocal register for koKR // {{{
 local L = Dcr.L;
 local LOC = Dcr.LOC;
 L:RegisterTranslations("koKR", function() return {
 
-
-	[LOC.CLASS_DRUID]	=	"드루이드",
-	[LOC.CLASS_HUNTER]	=	"사냥꾼",
-	[LOC.CLASS_MAGE]	=	"마법사",
-	[LOC.CLASS_PALADIN]	=	"성기사",
-	[LOC.CLASS_PRIEST]	=	"사제",
-	[LOC.CLASS_ROGUE]	=	"도적",
-	[LOC.CLASS_SHAMAN]	=	"주술사",
-	[LOC.CLASS_WARLOCK]	=	"흑마법사",
-	[LOC.CLASS_WARRIOR]	=	"전사",
-
-	[LOC.DISEASE]	=	"질병",
+  [LOC.DISEASE]	=	"질병",
 	[LOC.MAGIC]		=	"마법",
 	[LOC.POISON]	=	"독",
 	[LOC.CURSE]		=	"저주",
-	[LOC.MAGICCHARMED]	=	"주술", -- check
-	[LOC.CHARMED]	=	"지배", -- check
+	[LOC.MAGICCHARMED]	=	"마법 정화",
+	[LOC.CHARMED]	=	"변이",
 
-	-------------------------------------------------------------------------------
-	-- Korean localization
-	-------------------------------------------------------------------------------
-
-	--start added in Rc4
 
 	[LOC.CLASS_DRUID]	=	"드루이드",
 	[LOC.CLASS_HUNTER]	=	"사냥꾼",
@@ -68,6 +55,7 @@ L:RegisterTranslations("koKR", function() return {
 	[LOC.CLASS_SHAMAN]	=	"주술사",
 	[LOC.CLASS_WARLOCK]	=	"흑마법사",
 	[LOC.CLASS_WARRIOR]	=	"전사",
+	[LOC.CLASS_DEATHKNIGHT] = "죽음의 기사",
 
 	[LOC.STR_OTHER]	= 	"기타",
 	[LOC.STR_OPTIONS]	= 	"Decursive 설정",
@@ -87,11 +75,30 @@ L:RegisterTranslations("koKR", function() return {
 	[LOC.CLEAR_PRIO]	= "C",
 	[LOC.CLEAR_SKIP]	= "C",
 
-	[LOC.PET_FEL_CAST]	=	"마법 삼키기",
-	[LOC.PET_DOOM_CAST]	=	"마법 무효화",
 
+
+
+--[[
+LOC.PET_FEL_CAST  = "Devour Magic"; -- NO NEED TO TRANSLATE
+LOC.PET_DOOM_CAST = "Dispel Magic"; -- NO NEED TO TRANSLATE
+
+
+LOC.SPELL_POLYMORPH	      = 'Polymorph'; -- NO NEED TO TRANSLATE
+LOC.SPELL_CURE_DISEASE        = 'Cure Disease'; -- NO NEED TO TRANSLATE
+LOC.SPELL_ABOLISH_DISEASE     = 'Abolish Disease'; -- NO NEED TO TRANSLATE
+LOC.SPELL_PURIFY              = 'Purify'; -- NO NEED TO TRANSLATE
+LOC.SPELL_CLEANSE             = 'Cleanse'; -- NO NEED TO TRANSLATE
+LOC.SPELL_DISPELL_MAGIC       = 'Dispel Magic'; -- NO NEED TO TRANSLATE
+LOC.SPELL_CURE_POISON         = 'Cure Poison'; -- NO NEED TO TRANSLATE
+LOC.SPELL_ABOLISH_POISON      = 'Abolish Poison';-- NO NEED TO TRANSLATE
+LOC.SPELL_REMOVE_LESSER_CURSE = 'Remove Lesser Curse'; -- NO NEED TO TRANSLATE
+LOC.SPELL_REMOVE_CURSE        = 'Remove Curse'; -- NO NEED TO TRANSLATE
+LOC.SPELL_PURGE               = 'Purge'; -- NO NEED TO TRANSLATE
+LOC.SPELL_CYCLONE	      = 'Cyclone'; -- NO NEED TO TRANSLATE
+LOC.CLEANSE_SPIRIT	      = "Cleanse Spirit"; -- NO NEED TO TRANSLATE
+--]]
 	[BINDING_NAME_DCRSHOW]    	= "Decursive 메인바 표시/숨김\n(실시간 목록 고정위치)",
-    
+
 	[BINDING_NAME_DCRMUFSHOWHIDE] =	"작은 유닛 프레임 표시/숨김",
 
 	[BINDING_NAME_DCRPRADD]     = "대상을 우선순위 목록에 추가",
@@ -103,9 +110,7 @@ L:RegisterTranslations("koKR", function() return {
 	[BINDING_NAME_DCRSKCLEAR] 	= "제외 목록 초기화",
 	[BINDING_NAME_DCRSKLIST]  	= "제외 목록 출력",
 	[BINDING_NAME_DCRSKSHOW]  	= "제외 목록 표시/숨김",
-    
 	[BINDING_NAME_DCRSHOWOPTION]=	"고정창 옵션 표시",
-
 
 
 	[LOC.PRIORITY_LIST]  	= "Decursive 우선순위 목록",
@@ -123,8 +128,10 @@ L:RegisterTranslations("koKR", function() return {
 
 	[LOC.SHOW_TOOLTIP]    	= "툴팁 표시",
 	[LOC.REVERSE_LIVELIST]	= "실시간 목록 표시 반전",
-	[LOC.TIE_LIVELIST]    	= "실시간 목록 표시를 DCR 창과 함께 표시",
 	[LOC.HIDE_LIVELIST]   	= "실시간 목록 숨김",
+	[LOC.TIE_LIVELIST]    	= "실시간 목록 표시를 DCR 창과 함께 표시",
+  [LOC.OPT_CREATE_VIRTUAL_DEBUFF] = "가상 테스트 디버프 생성",
+	[LOC.OPT_CREATE_VIRTUAL_DEBUFF_DESC] = "디버프 발생 시 어떻게 보여지는 지를 표시하도록 합니다.",
 
 	[LOC.AMOUNT_AFFLIC]   	= "표시할 대상의 수 : ",
 	[LOC.BLACK_LENGTH]    	= "블랙리스트 추가 시간(초) : ",
@@ -137,14 +144,32 @@ L:RegisterTranslations("koKR", function() return {
 	[LOC.ANCHOR]          	= "Decursive 글자 위치",
 	[LOC.DONOT_BL_PRIO]   	= "우선순위 블랙리스트 제외",
 
-	-- $s is spell name
-	-- $a is affliction name/type
-	-- $t is target name
 	[LOC.SPELL_FOUND]      	= "%s 주문 발견!",
 
-	
-	-- NEW LOCALS FOR 2.0
-	[LOC.DEFAULT_MACROKEY] = "NONE", -- Ideally the key just beneath the "escape" key. Leave to "NONE" (do not translate) if you don't set a real key name.
+
+--[[
+-- spells and potions
+LOC.DREAMLESSSLEEP	= "Dreamless Sleep";		-- NO NEED TO TRANSLATE 
+LOC.GDREAMLESSSLEEP	= "Greater Dreamless Sleep";	-- NO NEED TO TRANSLATE 
+LOC.MDREAMLESSSLEEP	= "Major Dreamless Sleep";	-- NO NEED TO TRANSLATE
+LOC.ANCIENTHYSTERIA	= "Ancient Hysteria";		-- NO NEED TO TRANSLATE
+LOC.IGNITE		= "Ignite Mana";		-- NO NEED TO TRANSLATE
+LOC.TAINTEDMIND		= "Tainted Mind";		-- NO NEED TO TRANSLATE
+LOC.MAGMASHAKLES	= "Magma Shackles";		-- NO NEED TO TRANSLATE
+LOC.CRIPLES		= "Cripple";			-- NO NEED TO TRANSLATE
+LOC.DUSTCLOUD		= "Dust Cloud";			-- NO NEED TO TRANSLATE
+LOC.WIDOWSEMBRACE	= "Widow's Embrace";		-- NO NEED TO TRANSLATE
+LOC.CURSEOFTONGUES	= "Curse of Tongues";		-- NO NEED TO TRANSLATE
+LOC.SONICBURST		= "Sonic Burst";		-- NO NEED TO TRANSLATE
+LOC.THUNDERCLAP		= "Thunderclap";		-- NO NEED TO TRANSLATE -- unused
+LOC.DELUSIONOFJINDO	= "Delusions of Jin'do";	-- NO NEED TO TRANSLATE
+LOC.MUTATINGINJECTION	= "Mutating Injection";		-- NO NEED TO TRANSLATE
+LOC.DCR_LOC_SILENCE	= "Silence";			-- NO NEED TO TRANSLATE
+LOC.DCR_LOC_MINDVISION	= "Mind Vision";		-- NO NEED TO TRANSLATE
+--]]
+-- NEW LOCALS FOR 2.0
+
+
 	[LOC.OPT_LIVELIST] = "실시간 목록",
 	[LOC.OPT_LIVELIST_DESC] = "실시간 목록에 대한 설정입니다.",
 	[LOC.OPT_HIDELIVELIST_DESC] = "숨긴다면 해제된 대상의 정보를 표시합니다.",
@@ -155,8 +180,6 @@ L:RegisterTranslations("koKR", function() return {
 	[LOC.OPT_SCANLENGTH_DESC] = "각 탐색의 시간 간격을 지정합니다.",
 	[LOC.OPT_REVERSE_LIVELIST_DESC] = "실시간 목록을 아래에서 위로 생성합니다.",
 	[LOC.OPT_TIE_LIVELIST_DESC] = "실시간 목록을 아래에서 위로 생성합니다.",
-	[LOC.OPT_CREATE_VIRTUAL_DEBUFF] = "시각적인 테스트 디버프 생성",
-	[LOC.OPT_CREATE_VIRTUAL_DEBUFF_DESC] = "디버프 발생 시 어떻게 보여지는 지를 표시하도록 합니다.",
 	[LOC.OPT_MESSAGES]	= "메세지",
 	[LOC.OPT_MESSAGES_DESC] = "메세지 표시에 대한 설정입니다.",
 	[LOC.OPT_CHATFRAME_DESC] = "Decursive의 메세지가 기본 대화창에 표시됩니다.",
@@ -166,8 +189,25 @@ L:RegisterTranslations("koKR", function() return {
 	[LOC.OPT_MFSETTINGS] = "작은 유닛 프레임 설정",
 	[LOC.OPT_MFSETTINGS_DESC] = "작은 유닛 프레임에 대한 설정입니다.",
 	[LOC.OPT_DISPLAYOPTIONS] = "디스플레이 옵션",
-	[LOC.OPT_SHOWMFS] = "작은 유닛 프레임 표시",
+
+	[LOC.OPT_SHOWMFS] = "작은 유닛 프레임(MUF) 표시",
 	[LOC.OPT_SHOWMFS_DESC] = "클릭으로 해제하려면 반드시 활성화 되어야 합니다.",
+
+  [LOC.OPT_AUTOHIDEMFS]	    = "자동숨김",
+  [LOC.OPT_AUTOHIDEMFS_DESC]    = "선택하면 언제 MUF창을 숨겨둘지 설정합니다.",
+
+  [LOC.OPT_HIDEMFS_SOLO]	    = "솔로",
+  [LOC.OPT_HIDEMFS_SOLO_DESC]	    = "MUF창을 파티나 공격대에 속해있지 않을 때 숨겨둡니다.",
+
+  [LOC.OPT_HIDEMFS_GROUP]	    = "솔로/파티",
+  [LOC.OPT_HIDEMFS_GROUP_DESC]    = "MUF창을 공격대에 속해있지 않을 때 숨겨둡니다.",
+
+  [LOC.OPT_HIDEMFS_NEVER]	    = "사용안함",
+  [LOC.OPT_HIDEMFS_NEVER_DESC]    = "MUF창의 자동숨김을 사용하지 않습니다.",
+
+  [LOC.OPT_ADDDEBUFFFHIST]		= "최근의 디버프 추가",
+  [LOC.OPT_ADDDEBUFFFHIST_DESC]	= "예전에 사용된 디버프를 추가합니다.",
+
 	[LOC.OPT_GROWDIRECTION] = "MUF 표시 반전",
 	[LOC.OPT_GROWDIRECTION_DESC] = "MUF를 하단에서 상단으로 표시합니다.",
 
@@ -178,25 +218,13 @@ L:RegisterTranslations("koKR", function() return {
 	[LOC.MISSINGUNIT]		= "잘못된 대상",
 	[LOC.COLORALERT]		= "'%s'이 필요할때의 알림 색상을 설정합니다.",
 	[LOC.COLORSTATUS]		= "MUF 상태가 '%s'일때 색상을 설정합니다.",
-	[LOC.COLORCHRONOS]		= LOC.COLORCHRONOS,
-	[LOC.COLORCHRONOS_DESC]		= LOC.COLORCHRONOS_DESC,
-
-    [LOC.OPT_AUTOHIDEMFS]	    = LOC.OPT_AUTOHIDEMFS,
-    [LOC.OPT_AUTOHIDEMFS_DESC]	    = LOC.OPT_AUTOHIDEMFS_DESC,
-    [LOC.OPT_HIDEMFS_SOLO]	    = LOC.OPT_HIDEMFS_SOLO,
-    [LOC.OPT_HIDEMFS_SOLO_DESC]	    = LOC.OPT_HIDEMFS_SOLO_DESC,
-    [LOC.OPT_HIDEMFS_GROUP]	    = LOC.OPT_HIDEMFS_GROUP,
-    [LOC.OPT_HIDEMFS_GROUP_DESC]    = LOC.OPT_HIDEMFS_GROUP_DESC,
-    [LOC.OPT_HIDEMFS_NEVER]	    = LOC.OPT_HIDEMFS_NEVER,
-    [LOC.OPT_HIDEMFS_NEVER_DESC]    = LOC.OPT_HIDEMFS_NEVER_DESC,
-
-    [LOC.OPT_ADDDEBUFFFHIST]		= LOC.OPT_ADDDEBUFFFHIST,
-    [LOC.OPT_ADDDEBUFFFHIST_DESC]	= LOC.OPT_ADDDEBUFFFHIST_DESC,
+	[LOC.COLORCHRONOS]		= "크로노미터",
+	[LOC.COLORCHRONOS_DESC]		= "크로노미터의 색상을 설정합니다.",
 
 	[LOC.OPT_SHOWBORDER] = "직업 색상 테두리 표시",
 	[LOC.OPT_SHOWBORDER_DESC] = "MUF에 유닛의 직업에 따른 색상을 테두리로 표시합니다.",
-    [LOC.OPT_SHOWCHRONO] = LOC.OPT_SHOWCHRONO,
-    [LOC.OPT_SHOWCHRONO_DESC] = LOC.OPT_SHOWCHRONO_DESC,
+  [LOC.OPT_SHOWCHRONO] = "크로노미터 표시",
+  [LOC.OPT_SHOWCHRONO_DESC] = "유닛이 주문에 걸린 이후 경과된 초의 숫자를 표시합니다.",
 	[LOC.OPT_MAXMFS]  = "표시할 최대 유닛",
 	[LOC.OPT_MAXMFS_DESC] = "표시할 작은 유닛 프레임의 최대 개수를 지정합니다.",
 	[LOC.OPT_UNITPERLINES] = "한줄에 표시할 유닛의 수",
@@ -222,7 +250,7 @@ L:RegisterTranslations("koKR", function() return {
 	[LOC.OPT_CURINGORDEROPTIONS] = "해제 순서 설정",
 	[LOC.OPT_MAGICCHECK_DESC]	= "체크 시 마법에 걸린 대상을 표시하고 치료합니다.",
 	[LOC.OPT_MAGICCHARMEDCHECK_DESC] = "체크 시 지배에 걸린 대상을 표시하고 치료합니다.",
-	[LOC.OPT_CHARMEDCHECK_DESC] = "체크 시 지배에 걸린 대상을 표시하고 치료합니다.",
+	[LOC.OPT_CHARMEDCHECK_DESC] = "체크 시 지배에 걸린 대상을 표시하고 변이합니다.",
 	[LOC.OPT_POISONCHECK_DESC] = "체크 시 독에 걸린 대상을 표시하고 치료합니다.",
 	[LOC.OPT_DISEASECHECK_DESC] = "체크 시 질병에 걸린 대상을 표시하고 치료합니다.",
 	[LOC.OPT_CURSECHECK_DESC] = "체크 시 저주에 걸린 대상을 표시하고 치료합니다.",
@@ -242,7 +270,7 @@ L:RegisterTranslations("koKR", function() return {
 	[LOC.OPT_ALWAYSIGNORE] = "비전투시에도 항상 무시",
 	[LOC.OPT_ALWAYSIGNORE_DESC] = "선택시, 해당 디버프는 전투 중이 아닐 때에도 무시됩니다.",
 	[LOC.OPT_REMOVETHISDEBUFF] = "해당 디버프 제거",
-	[LOC.OPT_REMOVETHISDEBUFF_DESC] = "Skip 목록에서 '%s' 제거",
+	[LOC.OPT_REMOVETHISDEBUFF_DESC] = "제외 목록에서 '%s' 제거",
 	[LOC.OPT_RESETDEBUFF] = "해당 디버프 초기화",
 	[LOC.OPT_RESETDTDCRDEFAULT] = "'%s' Decursive 기본으로 초기화",
 	[LOC.OPT_USERDEBUFF] = "해당 디버프는 Decursive의 기본 디버프가 아닙니다.",
@@ -263,11 +291,10 @@ L:RegisterTranslations("koKR", function() return {
 	[LOC.OPT_LLALPHA] = "실시간 목록 투명도",
 	[LOC.OPT_LLALPHA_DESC] = "Decursive 메인바와 실시간 목록의 투명도를 변경합니다. (메인바가 표시되어 있어야 함)",
 
-   
 	[LOC.OPT_ADVDISP] = "고급 표시 설정",
 	[LOC.OPT_ADVDISP_DESC] = "각 MUF 사이 간격 설정을 위해 테두리와 가운데 구분의 투명도를 설정할 수 있습니다.",
 	[LOC.OPT_TIECENTERANDBORDER] = "가운데와 테두리의 투명도",
-	[LOC.OPT_TIECENTERANDBORDER_OPT] = "체크 시 테두리의 투명도가 가운데 투명도의 반이됩니다.",
+	[LOC.OPT_TIECENTERANDBORDER_OPT] = "체크 시 테두리의 투명도가 가운데 투명도의 절반이 됩니다.",
 	[LOC.OPT_BORDERTRANSP] = "테두리 투명도",
 	[LOC.OPT_BORDERTRANSP_DESC] = "테두리의 투명도를 설정합니다.",
 	[LOC.OPT_CENTERTRANSP] = "가운데 투명도",
@@ -275,11 +302,13 @@ L:RegisterTranslations("koKR", function() return {
 	[LOC.OPT_TIEXYSPACING] = "수평/수직 간격",
 	[LOC.OPT_TIEXYSPACING_DESC] = "MUF의 수평과 수직 간격이 같아 집니다.",
 	[LOC.OPT_XSPACING] = "수평 간격",
-	[LOC.OPT_XSPACING_DESC] = "MUF 사이의 수평 간격을 설정하니다.",
+	[LOC.OPT_XSPACING_DESC] = "MUF 사이의 수평 간격을 설정합니다.",
 	[LOC.OPT_YSPACING] = "수직 간격",
 	[LOC.OPT_YSPACING_DESC] = "MUF 사이의 수직 간격을 설정합니다.",
-    [LOC.OPT_SHOWMINIMAPICON] = LOC.OPT_SHOWMINIMAPICON,
-    [LOC.OPT_SHOWMINIMAPICON_DESC] = LOC.OPT_SHOWMINIMAPICON_DESC,
+
+  [LOC.OPT_SHOWMINIMAPICON] = "미니맵 아이콘",
+  [LOC.OPT_SHOWMINIMAPICON_DESC] = "미니맵 아이콘을 표시합니다.",
+
 
 
 	[LOC.HLP_LL_ONCLICK_TEXT] = "실시간 목록을 클릭하는 것은 WoW 2.0 이후 사용할 수 없습니다. Decursive 폴더에 위치한 \"Readme.txt\" 읽어 보세요...\n(해당 목록을 이동하려면 /dcrshow 혹은 ALT-좌-클릭하세요.)",
@@ -290,13 +319,13 @@ L:RegisterTranslations("koKR", function() return {
 	[LOC.HLP_NOTHINGTOCURE] = "치료할 것이 없습니다!",
 	[LOC.HLP_WRONGMBUTTON] = "잘못된 마우스 버튼입니다!",
 	[LOC.HLP_USEXBUTTONTOCURE] = "해당 디버프를 치료하려면 \"%s\" 버튼을 사용하세요",
-
-	[LOC.CTRL] 	= "CTRL",
-	[LOC.ALT] 	= "ALT",
-	[LOC.SHIFT] = "SHIFT",
+  
+	[LOC.CTRL] 	= "Ctrl",
+	[LOC.ALT] 	= "Alt",
+	[LOC.SHIFT] = "Shift",
 
 	[LOC.TARGETUNIT] 	= "대상",
-	[LOC.FOCUSUNIT] 	= "주시 대상",
+	[LOC.FOCUSUNIT] 	= "주시대상",
 
 	[LOC.ABSENT] 	= "(%s) 자리비움",
 	[LOC.TOOFAR] 	= "거리 벗어남",
@@ -307,23 +336,30 @@ L:RegisterTranslations("koKR", function() return {
 	[LOC.AFFLICTEDBY] 	= "%s에 걸림",
 
 	[LOC.SUCCESSCAST] 	= "|cFF22FFFF%s %s|r|1으로;로; %s |cFF00AA00치료 성공!|r",
-	[LOC.FAILEDCAST]	= LOC.FAILEDCAST,
-	[LOC.HANDLEHELP] 	= "작은 유닛 프레임(MUFs) 모두 이동",
+	[LOC.FAILEDCAST]	= "|cFF22FFFF%s %s|r|1으로;로; %s |cFFAA0000치료 실패|r\n|cFF00AAAA%s|r",
+
+	[LOC.HANDLEHELP] 	= "작은 유닛 프레임(MUF) 모두 이동",
 
 	[LOC.MACROKEYALREADYMAPPED] = "경고: Decursive 매크로에 지정한 [%s]키는 '%s'를 위해 지정되어 있었습니다.\n당신이 매크로에 다른 키를 지정하면 Decursive는 이전 설정을 복원할 것입니다.",
 	[LOC.MACROKEYMAPPINGSUCCESS] = "[%s] 키가 Decursive 매크로로 성공적으로 지정되었습니다.",
 	[LOC.MACROKEYMAPPINGFAILED] = "[%s] 키는 Decursive 매크로로 지정할 수 없습니다!",
 	[LOC.MACROKEYNOTMAPPED] = "Decursive 매크로가 지정되지 않았습니다. 설정 메뉴를 통해 키를 지정할 수 있습니다.",
 
+	[LOC.DEFAULT_MACROKEY] = "NONE", -- Ideally the key just beneath the "escape" key. Leave to "NONE" (do not translate) if you don't set a real key name.
 	[LOC.OPT_NOKEYWARN] = "키 없음 경고",
 	[LOC.OPT_NOKEYWARN_DESC] = "지정된 키가 없다면 경고를 표시합니다.",
 	[LOC.NOSPELL] = "이용가능한 주문이 없습니다.",
 
 	[LOC.FUBARMENU]  = "FuBar 메뉴",
 	[LOC.FUBARMENU_DESC]  = "FuBar 아이콘에 관련된 옵션을 설정합니다.",
-    
-    -- OPTIONS
 
+--[[
+  [LOC.GLOR1] = "In memory of Glorfindal",
+  [LOC.GLOR2] = "Decursive is dedicated to the memory of Bertrand who left way too soon.\nHe will always be remembered.",
+  [LOC.GLOR3] = "In remembrance of Bertrand Sense\n1969 - 2007",
+  [LOC.GLOR4] =  "Friendship and affection can take their roots anywhere, those who met Glorfindal in World of Warcraft knew a man of great commitment and a charismatic leader.\n\nHe was in life as he was in game, selfless, generous, dedicated to his friends and most of all, a passionate man.\n\nHe left us at the age of 38 leaving behind him not just anonymous players in a virtual world but, a group of true friends who will miss him forever.",
+  [LOC.GLOR5] = "He will always be remembered...",
+--]]
 } end);
 
 -- // }}}
