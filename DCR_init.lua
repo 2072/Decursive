a96f8721ca82f7c044e83640a7cc1f6dda25971f
Archarodim <Archarodim@teaser.fr>
2009-08-26 11:34:37 +0200

    - Added ALL the simple keyword to see the ones that are actually replaced by the packager

--- DCR_init.lua
+++ DCR_init.lua
@@ -1256,3 +1256,51 @@ D.version = "@project-version@";
 DcrLoadedFiles["DCR_init.lua"] = "@project-version@";
 
 -------------------------------------------------------------------------------
+
+--[======[
+TEST to see what keyword substitutions are actually working.... DAMN!!!!
+
+
+Simple replacements
+
+@file-revision@
+    Turns into the current revision of the file in integer form. e.g. 1234
+    Note: does not work for git
+@project-revision@
+    Turns into the highest revision of the entire project in integer form. e.g. 1234
+    Note: does not work for git
+@file-hash@
+    Turns into the hash of the file in hex form. e.g. 106c634df4b3dd4691bf24e148a23e9af35165ea
+    Note: does not work for svn
+@project-hash@
+    Turns into the hash of the entire project in hex form. e.g. 106c634df4b3dd4691bf24e148a23e9af35165ea
+    Note: does not work for svn
+@file-abbreviated-hash@
+    Turns into the abbreviated hash of the file in hex form. e.g. 106c63 Note: does not work for svn
+@project-abbreviated-hash@
+    Turns into the abbreviated hash of the entire project in hex form. e.g. 106c63
+    Note: does not work for svn
+@file-author@
+    Turns into the last author of the file. e.g. ckknight
+@project-author@
+    Turns into the last author of the entire project. e.g. ckknight
+@file-date-iso@
+    Turns into the last changed date (by UTC) of the file in ISO 8601. e.g. 2008-05-01T12:34:56Z
+@project-date-iso@
+    Turns into the last changed date (by UTC) of the entire project in ISO 8601. e.g. 2008-05-01T12:34:56Z
+@file-date-integer@
+    Turns into the last changed date (by UTC) of the file in a readable integer fashion. e.g. 20080501123456
+@project-date-integer@
+    Turns into the last changed date (by UTC) of the entire project in a readable integer fashion. e.g. 2008050123456
+@file-timestamp@
+    Turns into the last changed date (by UTC) of the file in POSIX timestamp. e.g. 1209663296
+@project-timestamp@
+    Turns into the last changed date (by UTC) of the entire project in POSIX timestamp. e.g. 1209663296
+@project-version@
+    Turns into an approximate version of the project. The tag name if on a tag, otherwise it's up to the repo.
+    :SVN returns something like "r1234"
+    :Git returns something like "v0.1-873fc1"
+    :Mercurial returns something like "r1234". 
+
+
+--]======]
