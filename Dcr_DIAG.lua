2c906bf1d11e354a41064ce1807722a69ceccf3a
Archarodim <Archarodim@teaser.fr>
2010-11-24 06:37:41 +0100

    - Added a message when Decursive's installation appears to be corrupted to prompt the user to install BugGrabber and BugSack.

--- Dcr_DIAG.lua
+++ Dcr_DIAG.lua
@@ -288,7 +288,7 @@ do
         };
 
         local GenericErrorMessage1 = "Decursive could not initialize properly because one or several of the required shared libraries (at least |cFF00FF00AceLibrary or LibStub|r) could not be found.\n";
-        local GenericErrorMessage2 = "Try to re-install Decursive from its original archive or use the |cFF00FF00Curse client|r (Curse.com) to update |cFFFF0000ALL|r your add-ons properly.";
+        local GenericErrorMessage2 = "Try to re-install Decursive from its original archive or use the |cFF00FF00Curse client|r (Curse.com) to update |cFFFF0000ALL|r your add-ons properly.\nIf that doesn't work, install the add-ons BugGrabber and BugSack in order to detect other errors preventing Decursive to load.";
 
         local ErrorFound = false;
         local Errors = {};
