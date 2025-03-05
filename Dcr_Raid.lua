8a406563c5b5e760504cea6ca0823cb0d2abe877
Archarodim <john.wellesz@gmail.com>
2025-03-05 10:29:56 +0100

    Enable role detection in non retail versions of WoW

--- Dcr_Raid.lua
+++ Dcr_Raid.lua
@@ -233,9 +233,9 @@ do
     local FakeRoles = {}; local roles = {"HEALER", "TANK", "DAMAGER", "NONE"};
     local function _UnitGroupRolesAssigned(unit)
 
-        if DC.WOWC then
-            return "NONE";
-        end
+        -- if DC.WOWC then
+        --    return "NONE";
+        -- end
 
         if not TestMode then
             return UnitGroupRolesAssigned(unit);
