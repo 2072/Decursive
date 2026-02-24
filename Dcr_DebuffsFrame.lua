5511fe04cec03b6dcd502913fc5cba6a316a51d0
bozoweed <bozoweed@gmail.com>
2026-02-24 10:00:38 +0100

    properties acces is faster than dynamical key index

--- Dcr_DebuffsFrame.lua
+++ Dcr_DebuffsFrame.lua
@@ -1668,7 +1668,8 @@ do
             self.Texture:SetColorTexture(self.Color[1], self.Color[2], self.Color[3], Alpha);
 
             if DC.MN and debuff_1 and debuff_1.secretMode and debuff_1.s_color then
-                self.Texture:SetColorTexture(debuff_1.s_color["r"], debuff_1.s_color["g"], debuff_1.s_color["b"], Alpha);
+                local color = debuff_1.s_color
+                self.Texture:SetColorTexture(color.r, color.g, color.b, Alpha);
             end
             --self.Texture:SetAlpha(Alpha);
             --@debug@
